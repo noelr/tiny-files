03a67bb55123b9bf68824257188b060f3f71a1ff
Noel Rohrbach <nr@oxon.ch>
2016-12-21 13:50:35 +0100

    use scootty

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,87 +1,25 @@
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE TypeOperators         #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE RecordWildCards       #-}
-module Lib where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Network.Wai.Middleware.RequestLogger
-import Control.Monad.Trans.Resource
-import Control.Monad.Except
+module Lib (go) where
 
-import Data.Monoid
-import Data.Text (Text)
-import qualified Data.ByteString.Lazy as LS
-import qualified Data.Text as Text
-import qualified Data.Text.Encoding as Text
-import qualified Data.Text.IO as TIO
+import Web.Scotty
+
+import Control.Monad.IO.Class
 
 import Network.Wai.Parse
-import Network.Wai.Handler.Warp hiding (FileInfo)
-import Servant
-import Servant.Server.Internal
-import System.Directory
-import System.FilePath
 
-data Mem
-data Tmp
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Char8 as BS
+import System.FilePath ((</>))
 
-class KnownBackend b where
-  type Storage b :: * -- associated type family
-
-  withBackend :: Proxy b -> (BackEnd (Storage b) -> IO r) -> IO r
-
-instance KnownBackend Mem where
-  type Storage Mem = LS.ByteString
-
-  withBackend Proxy f = f lbsBackEnd
-
-instance KnownBackend Tmp where
-  type Storage Tmp = FilePath
-
-  withBackend Proxy f = runResourceT . withInternalState $ \s ->
-    f (tempFileBackEnd s)
-
-data Files b
-
-type MultiPartData b = ([Param], [File (Storage b)])
-
-instance (KnownBackend b, HasServer sublayout config)
-      => HasServer (Files b :> sublayout) config where
-
-  type ServerT (Files b :> sublayout) m =
-    IO (MultiPartData b) -> ServerT sublayout m
-
-  route Proxy config subserver =
-    route psub config (addBodyCheck subserver check)
-    where
-      psub  = Proxy :: Proxy sublayout
-      pbak  = Proxy :: Proxy b
-      check = withRequest $ \request -> return $ withBackend pbak $
-        \backend -> parseRequestBody backend request
-
-type API = "upload" :> Files Mem :> Post '[PlainText] Text
-      :<|> Raw
-
--- | Handle the uploaded image and video files.
-upload :: IO (MultiPartData Mem) -> ExceptT ServantErr IO Text
-upload multipart = do
-  liftIO $ putStrLn "handling file upload..."
-  (params, files) <- liftIO multipart
-
-  liftIO $ mapM_ wrFile files
-  return "OK"
-  where
-    wrFile (_, FileInfo{..}) = LS.writeFile newFileName fileContent
-      where newFileName = (Text.unpack . Text.decodeUtf8) "static/download"
-
-
-server :: Server API
-server = upload
-  :<|> serveDirectory "./static"
-
-go :: IO ()
-go = run 8888 $ logStdoutDev (serve (Proxy :: Proxy API) server)
+go = scotty 3000 $ do
+  get "/" $ file "./static/index.html"
+  get "/download" $ do
+    setHeader "content-disposition" "attachment"
+    file "./static/file"
+  post "/upload" $ do
+      fs <- files
+      let fs' = [ (fieldName, BS.unpack (fileName fi), fileContent fi) | (fieldName,fi) <- fs ]
+      -- write the files to disk, so they will be served by the static middleware
+      liftIO $ sequence_ [ B.writeFile ("static/file") fc | (_,_,fc) <- fs' ]
+      text "OK"
