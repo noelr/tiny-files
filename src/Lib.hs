0bf0b59d5b3e54873a80dcdfc9fbdb3db7e84ac6
Noel Rohrbach <nr@oxon.ch>
2016-12-21 14:06:45 +0100

    add logger

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,19 +7,21 @@ import Web.Scotty
 import Control.Monad.IO.Class
 
 import Network.Wai.Parse
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Char8 as BS
 import System.FilePath ((</>))
 
 go = scotty 3000 $ do
+  middleware logStdoutDev
   get "/" $ file "./static/index.html"
   get "/download" $ do
     setHeader "content-disposition" "attachment"
-    file "./static/file"
+    file "static/file"
   post "/upload" $ do
       fs <- files
       let fs' = [ (fieldName, BS.unpack (fileName fi), fileContent fi) | (fieldName,fi) <- fs ]
-      -- write the files to disk, so they will be served by the static middleware
-      liftIO $ sequence_ [ B.writeFile ("static/file") fc | (_,_,fc) <- fs' ]
+      -- write the files to disk
+      liftIO $ sequence_ [ B.writeFile "static/file" fc | (_,_,fc) <- fs' ]
       text "OK"
