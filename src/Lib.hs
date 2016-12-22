a11428e0448622ea7eb02dd4f0b30b56d949b138
Noel Rohrbach <nr@oxon.ch>
2016-12-22 16:27:44 +0100

    keep multiple files

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Lib (go) where
 
@@ -12,17 +13,29 @@ import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Char8 as BS
 import System.FilePath ((</>))
+import System.Directory
+
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics
+
+data UserFile = UserFile { name :: FilePath } deriving (Show, Generic)
+instance ToJSON UserFile
 
 go = scotty 4001 $ do
   middleware logStdoutDev
   get "/" $ file "static/index.html"
+  get "/files" $ do
+    f <- liftIO $ listDirectory "download"
+    let fs = fmap UserFile f
+    json fs
   get "/main.js" $ file "static/main.js"
-  get "/download" $ do
+  get "/download/:file" $ do
+    name <- param "file"
     setHeader "content-disposition" "attachment"
-    file "static/file"
+    file $ "static/" ++ name
   post "/upload" $ do
       fs <- files
       let fs' = [ (fieldName, BS.unpack (fileName fi), fileContent fi) | (fieldName,fi) <- fs ]
       -- write the files to disk
-      liftIO $ sequence_ [ B.writeFile "static/file" fc | (_,_,fc) <- fs' ]
+      liftIO $ sequence_ [ B.writeFile ("download/" ++ fn) fc | (_,fn,fc) <- fs' ]
       text "OK"
